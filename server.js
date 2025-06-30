bf4ff74c02fffaacbcdb14805a3c4aaee6ae7119
soueb111 <souzhichang@gmail.com>
2025-07-01 04:30:57 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import fetch from 'node-fetch';
-import { WebSocketServer } from 'ws';
+const fetch = require('node-fetch');
+const { WebSocketServer } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const wss = new WebSocketServer({ port: PORT });
